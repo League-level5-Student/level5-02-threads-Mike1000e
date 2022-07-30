6a765b23764b09a9eb98f1bcfc952e565e36275e
Mike1000e <56172395+Mike1000e@users.noreply.github.com>
2022-07-29 18:25:48 -0700

    save

--- src/_01_Olympic_Rings/OlympicRings_Threaded.java
+++ src/_01_Olympic_Rings/OlympicRings_Threaded.java
@@ -1,8 +1,33 @@
 package _01_Olympic_Rings;
 
+import org.jointheleague.graphical.robot.Robot;
 
 public class OlympicRings_Threaded {
 	// Make A Program that uses Threads and robots to draw the Olympic rings. One robot should draw one ring simultaneously with the other 4 robots.
-
+	
+	
+	public static void main(String[] args) {
+		Robot topLeft = new Robot(100,100);
+		Robot topMid = new Robot(100,100);
+		Robot topRight = new Robot(100,100);
+		Robot bottomLeft = new Robot(100,100);
+		Robot bottomRight = new Robot(100,100);
+		
+		topLeft.setSpeed(10);
+		topMid.setSpeed(10);
+		topRight.setSpeed(10);
+		bottomLeft.setSpeed(10);
+		bottomRight.setSpeed(10);
+		
+		Thread tl = new Thread(()->topLeft.moveTo(100, 100));
+		Thread tm = new Thread(()->topMid.moveTo(200, 100));
+		Thread tr = new Thread(()->topRight.moveTo(300, 100));
+		Thread bl = new Thread(()->bottomLeft.moveTo(150, 200));
+		Thread br = new Thread(()->bottomRight.moveTo(250, 200));
+		
+		Thread tl2 = new Thread(()->topLeft.move(0)
+		
+		tl.start();
+	}
 }
 
---
+++ src/_03_Threaded_Greeting/ThreadedGreeter.java
@@ -0,0 +1,11 @@
+package _03_Threaded_Greeting;
+
+public class ThreadedGreeter implements Runnable {
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
